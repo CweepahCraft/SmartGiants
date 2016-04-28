bda1f1a3968f39cb5c7b060dc714021d27400aa0
Jacob Martin <jjm_223@hotmail.com>
2016-04-27 19:28:15 -0500

    Skip use of reflection for natural spawning, as 1.9 does.

--- v1_8_R3/src/main/java/me/jjm_223/smartgiants/entities/v1_8_R3/NaturalSpawns.java
+++ v1_8_R3/src/main/java/me/jjm_223/smartgiants/entities/v1_8_R3/NaturalSpawns.java
@@ -5,8 +5,8 @@ import me.jjm_223.smartgiants.entities.v1_8_R3.nms.SmartGiant;
 import me.jjm_223.smartgiants.entities.v1_8_R3.nms.SmartGiantHostile;
 import net.minecraft.server.v1_8_R3.BiomeBase;
 import net.minecraft.server.v1_8_R3.BiomeBase.BiomeMeta;
+import net.minecraft.server.v1_8_R3.EnumCreatureType;
 
-import java.lang.reflect.Field;
 import java.util.List;
 
 public class NaturalSpawns implements INaturalSpawns {
@@ -29,62 +29,22 @@ public class NaturalSpawns implements INaturalSpawns {
     }
 
     public void daylight() {
-        try {
-            for (Field field : BiomeBase.class.getDeclaredFields()) {
-                if (field.getType().getSimpleName().equals(BiomeBase.class.getSimpleName())) {
-                    if (field.get(null) != null) {
-                        for (Field list : BiomeBase.class.getDeclaredFields()) {
-                            if (list.getType().getSimpleName().equals(List.class.getSimpleName())) {
-                                if (field.get(null) == BiomeBase.PLAINS
-                                        || field.get(null) == BiomeBase.ICE_PLAINS
-                                        || field.get(null) == BiomeBase.STONE_BEACH
-                                        || field.get(null) == BiomeBase.EXTREME_HILLS
-                                        || field.get(null) == BiomeBase.EXTREME_HILLS_PLUS
-                                        || field.get(null) == BiomeBase.DESERT
-                                        && list.getName().equals("au")) {
-                                    list.setAccessible(true);
-                                    List<BiomeMeta> metaList = (List) list.get(field.get(null));
-                                    if (hostile) {
-                                        metaList.add(new BiomeMeta(SmartGiantHostile.class, frequency, minGroupAmount,
-                                                maxGroupAmount));
-                                    } else {
-                                        metaList.add(new BiomeMeta(SmartGiant.class, frequency, minGroupAmount,
-                                                maxGroupAmount));
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+        for (BiomeBase biomeBase : BiomeBase.getBiomes()) {
+            List mobs = biomeBase.getMobs(EnumCreatureType.CREATURE);
+            if (!mobs.isEmpty()) {
+                mobs.add(new BiomeMeta(
+                        (hostile ? SmartGiantHostile.class : SmartGiant.class), frequency, minGroupAmount, maxGroupAmount));
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
     public void night() {
-        try {
-            for (Field field : BiomeBase.class.getDeclaredFields()) {
-                if (field.getType().getSimpleName().equals(BiomeBase.class.getSimpleName())) {
-                    field.setAccessible(true);
-                    if (field.get(null) != null) {
-                        if (!(field.get(null) == BiomeBase.MUSHROOM_ISLAND)) {
-                            Field atField = BiomeBase.class.getDeclaredField("at");
-                            atField.setAccessible(true);
-                            List at = (List) atField.get(field.get(null));
-                            if (hostile) {
-                                at.add(new BiomeMeta(SmartGiantHostile.class, frequency, minGroupAmount,
-                                        maxGroupAmount));
-                            } else {
-                                at.add(new BiomeMeta(SmartGiant.class, frequency, minGroupAmount,
-                                        maxGroupAmount));
-                            }
-                        }
-                    }
-                }
+        for (BiomeBase biomeBase : BiomeBase.getBiomes()) {
+            List mobs = biomeBase.getMobs(EnumCreatureType.MONSTER);
+            if (!mobs.isEmpty()) {
+                mobs.add(new BiomeMeta((hostile ? SmartGiantHostile.class : SmartGiant.class),
+                        frequency, minGroupAmount, maxGroupAmount));
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
