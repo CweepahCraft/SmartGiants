eb802cd42bd7bf2b887e7dd3d79e39cd88555110
Jacob Martin <jjm_223@hotmail.com>
2016-11-09 18:42:01 -0600

    Lang revamp. Old lang files will be updated automatically.

--- Plugin/src/main/java/me/jjm_223/smartgiants/Messages.java
+++ Plugin/src/main/java/me/jjm_223/smartgiants/Messages.java
@@ -1,60 +1,85 @@
 package me.jjm_223.smartgiants;
 
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.logging.Logger;
 
 public class Messages
 {
-    private static FileConfiguration config = new YamlConfiguration();
-    private static boolean isLoaded = false;
+    private static FileConfiguration config = null;
+    private static FileConfiguration fallback = null;
 
     public static String getLang(String section)
     {
-        return ChatColor.translateAlternateColorCodes('&', config.getString(section));
+        String string = config.getString(section);
+        if (string == null)
+        {
+            string = fallback.getString(section);
+        }
+
+        return ChatColor.translateAlternateColorCodes('&', string);
     }
 
     public static void loadMessages(SmartGiants plugin)
     {
-        File configFile = new File(plugin.getDataFolder(), "lang.yml");
-        try
-        {
-            config.load(configFile);
-            isLoaded = true;
-        }
-        catch (IOException e)
-        {
-            fallback();
-        }
-        catch (InvalidConfigurationException e)
-        {
-            fallback();
-        }
-    }
+        Logger logger = JavaPlugin.getPlugin(SmartGiants.class).getLogger();
 
-    private static void fallback()
-    {
+        fallback = new YamlConfiguration();
         InputStream stream = Messages.class.getClassLoader().getResourceAsStream("lang.yml");
         try
         {
-            config.load(stream);
-            stream.close();
-            isLoaded = true;
+            fallback.load(stream);
         }
-        catch (IOException e)
+        catch (IOException | InvalidConfigurationException e)
         {
+            logger.severe("Failed to read bundled lang file!");
             e.printStackTrace();
-            isLoaded = false;
         }
-        catch (InvalidConfigurationException e)
+
+        File configFile = new File(plugin.getDataFolder(), "lang.yml");
+        config = new YamlConfiguration();
+        try
         {
-            e.printStackTrace();
-            isLoaded = false;
+            config.load(configFile);
         }
+        catch (IOException | InvalidConfigurationException ex)
+        {
+            logger.severe("Failed to read current language file.");
+            ex.printStackTrace();
+        }
+
+        if (config != null && fallback != null)
+        {
+            try
+            {
+                updateCurrentLangFile(configFile);
+            }
+            catch (IOException ex)
+            {
+                ex.printStackTrace();
+                logger.severe("Failed to update current language file.");
+            }
+        }
+    }
+
+    private static void updateCurrentLangFile(File file) throws IOException
+    {
+        for (String string : fallback.getKeys(true))
+        {
+            if (!config.contains(string))
+            {
+                config.set(string, fallback.getString(string));
+            }
+        }
+
+        config.save(file);
     }
 }
