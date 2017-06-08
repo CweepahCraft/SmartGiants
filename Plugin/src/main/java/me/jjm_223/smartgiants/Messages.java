5f70ec17da7d69a49623036bd8baa8da83b80753
Jacob Martin <jjm_223@hotmail.com>
2017-06-08 16:16:53 -0500

    Stop using deprecated configuration loader
    It was removed in 1.12.

--- Plugin/src/main/java/me/jjm_223/smartgiants/Messages.java
+++ Plugin/src/main/java/me/jjm_223/smartgiants/Messages.java
@@ -9,6 +9,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.logging.Logger;
 
 public class Messages
@@ -35,7 +36,7 @@ public class Messages
         InputStream stream = Messages.class.getClassLoader().getResourceAsStream("lang.yml");
         try
         {
-            fallback.load(stream);
+            fallback.load(new InputStreamReader(stream));
         }
         catch (IOException | InvalidConfigurationException e)
         {
